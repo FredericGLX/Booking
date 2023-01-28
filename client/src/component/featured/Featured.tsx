0622b5f566cbb16bec1c29370a0483bab1349bff
Frederic Guilloux <frederic.guilloux85@gmail.com>
2023-01-28 22:51:04 +1300

    changed featured locations

--- client/src/component/featured/Featured.tsx
+++ client/src/component/featured/Featured.tsx
@@ -3,7 +3,7 @@ import './featured.scss';
 
 export default function Featured(): JSX.Element {
   const { data, loading, error } = useFetch(
-    '/hotels/countByCity?cities=berlin,madrid,london'
+    '/hotels/countByCity?cities=auckland,wellington,queenstown'
   );
 
   return (
